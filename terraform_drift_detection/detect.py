d3ce12c6599523f99c365df5832ee4c21ad28709
Jian <jian-feng@artsymail.com>
2023-02-01 14:59:14 -0500

    feat: add .env.example to describe script's inputs

--- terraform_drift_detection/detect.py
+++ terraform_drift_detection/detect.py
@@ -1,14 +1,5 @@
 #!/usr/bin/env python
 
-# Requirements:
-#
-# - AWS credentials for Terraform AWS Provider.
-#   https://registry.terraform.io/providers/hashicorp/aws/latest/docs
-#
-# - SSH key for cloning Github repos.
-#
-# - Repo list.
-
 import os
 import tempfile
 
@@ -16,14 +7,10 @@ from terraform_drift_detection.git import clone_repo
 from terraform_drift_detection.terraform import find_tf_dirs, tf_in_repo_dir
 from terraform_drift_detection.util import abort_on_nonzero_exit, getenv, run_cmd
 
-######
-# main
-######
-
-repos, github_key = getenv()
+repo_names, github_key = getenv()
 
 with tempfile.TemporaryDirectory() as tmpdir:
-  for repo in repos:
+  for repo in repo_names:
     print('INFO: checking repo: %s' %repo)
     clone_repo(repo, tmpdir)
     repo_dir = os.path.join(tmpdir, repo)
--- terraform_drift_detection/test/test_terraform.py
+++ terraform_drift_detection/test/test_terraform.py
@@ -3,7 +3,7 @@ import pytest
 from subprocess import CompletedProcess
 from terraform_drift_detection.terraform import find_tf_dirs, tf_in_repo_dir
 
-def describe_test_find_tf_dirs():
+def describe_find_tf_dirs():
   def it_finds(mocker):
     files = [
       '/foo/foo/foo.tf',
