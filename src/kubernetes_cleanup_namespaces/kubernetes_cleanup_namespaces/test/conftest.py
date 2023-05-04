a197381475dfcc3924d2303ee27681a0fbf94fee
Jian <jian-feng@artsymail.com>
2023-05-03 21:53:44 -0400

    fix: error when pytest called with dir as arg

--- src/kubernetes_cleanup_namespaces/kubernetes_cleanup_namespaces/test/conftest.py
+++ src/kubernetes_cleanup_namespaces/kubernetes_cleanup_namespaces/test/conftest.py
@@ -1,6 +1,18 @@
 import os
 import sys
 
-# prepare environment before tests
+## prepare environment before tests
+
+# set env
 os.environ['KUBECTL_CONTEXT'] = 'foo'
-sys.argv += ['10'] # corresponds to ndays command line argument
+
+# set ndays command line argument
+if len(sys.argv) == 2:
+  # indicates that pytest is called with a dir path as argument
+  # dir path becomes first argument to script
+  # over-write it
+  sys.argv[1] = '10'
+else:
+  # indicates that pytest is called with no argument
+  # add argument
+  sys.argv += ['10']
