99882802db94e048af3a8f67dbdc32783f16453a
Jian <jian-feng@artsymail.com>
2023-06-14 17:00:58 -0400

    feat: rename mock_boto3_client to mock_ecr_client

--- src/lib/test/test_ecr_interface.py
+++ src/lib/test/test_ecr_interface.py
@@ -1,31 +1,41 @@
-import boto3
+import lib.test.fixtures.ecr_interface
 
 from lib.ecr_interface import ECRInterface
 from lib.test.fixtures.ecr_interface import (
-  mock_boto3_client,
+  mock_ecr_client,
   mock_ecr_describe_repositories_result, # indirect
-  mock_ecr_list_tags_for_resource_result # indirect
+  mock_ecr_list_tags_for_resource_result, # indirect
 )
 
 def describe_ecr_interface():
-  def describe_get_repos():
-    def it_gets(mocker, mock_boto3_client):
+  def describe_instantiation():
+    def it_instantiates(mocker, mock_ecr_client):
       mocker.patch(
-        'lib.ecr_interface.boto3.client',
-        return_value=mock_boto3_client
+        'lib.ecr_interface.boto3client',
+        return_value=mock_ecr_client
+      )
+      spy = mocker.spy(lib.ecr_interface, 'boto3client')
+      ecr_interface = ECRInterface()
+      spy.assert_has_calls([mocker.call('ecr')])
+  def describe_get_repos():
+    def it_gets(mocker, mock_ecr_client):
+      mocker.patch(
+        'lib.ecr_interface.boto3client',
+        return_value=mock_ecr_client
       )
       ecr_interface = ECRInterface()
       repos = ecr_interface.get_repos()
       assert len(repos) == 2
       assert repos[0]['repositoryName'] == 'foo1'
   def describe_get_repo_tags():
-    def it_gets(mocker, mock_boto3_client):
+    def it_gets(mocker, mock_ecr_client):
       mocker.patch(
-        'lib.ecr_interface.boto3.client',
-        return_value=mock_boto3_client
+        'lib.ecr_interface.boto3client',
+        return_value=mock_ecr_client
       )
       ecr_interface = ECRInterface()
-      res = ecr_interface.get_repo_tags('foo1')
+      arn = 'arn:aws:ecr:us-east-1:123:repository/foo1'
+      res = ecr_interface.get_repo_tags(arn)
       assert res['tags'][0] == {
         'Key': 'env', 'Value': 'test'
       }
