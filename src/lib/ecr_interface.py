3a1a46a1dcd812c62215d539fb77a4e27e766c84
Jian <jian-feng@artsymail.com>
2023-06-14 14:42:02 -0400

    feat: make ECRInterface object ecr attribute private

--- src/lib/ecr_interface.py
+++ src/lib/ecr_interface.py
@@ -4,16 +4,16 @@ class ECRInterface(object):
   ''' interface with AWS ECR '''
 
   def __init__(self):
-    self.ecr = boto3.client('ecr')
+    self._ecr = boto3.client('ecr')
 
   def get_repos(self):
     repos = []
-    res = self.ecr.describe_repositories()
+    res = self._ecr.describe_repositories()
     repos += res['repositories']
     while 'nextToken' in res:
-      res = self.ecr.describe_repositories(nextToken=res['nextToken'])
+      res = self._ecr.describe_repositories(nextToken=res['nextToken'])
       repos += res['repositories']
     return repos
 
   def get_repo_tags(self, arn):
-    return self.ecr.list_tags_for_resource(resourceArn=arn)
+    return self._ecr.list_tags_for_resource(resourceArn=arn)
