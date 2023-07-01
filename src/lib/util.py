84d3a3fa67a33afbaeea6084224642bc08c45580
Jian <jian-feng@artsymail.com>
2023-07-01 13:16:30 -0400

    feat: update lib/util tests

--- src/lib/util.py
+++ src/lib/util.py
@@ -25,21 +25,32 @@ def list_match_str(list_a, str1):
   ''' return elements of list_a that match string str1 '''
   return [x for x in list_a if str1 in x]
 
-def run_cmd(cmd, dirx):
+def parse_string_of_key_value_pairs(str1):
+  '''
+  given 'foo:x,foo:y,bar:x',
+  return {'foo': ['x', 'y'], 'bar': ['x']}
+  '''
+  dict1 = {}
+  for kv in str1.split(','):
+    k, v = kv.split(':')
+    dict1[k] = dict1.get(k, []) + [v]
+  return dict1
+
+def run_cmd(cmd, dirx, timeout=300):
   ''' run command in dir and return output '''
   os.chdir(dirx)
   logging.info(f"running {cmd} command in {dirx} directory.")
-  output = subprocess.run(
+  resp = subprocess.run(
     cmd,
     capture_output=True,
     shell=True,
     text=True,
-    timeout=300
+    timeout=timeout
   )
   logging.info(
-    f"{cmd} command exited with code {output.returncode}"
+    f"{cmd} command exited with code {resp.returncode}"
   )
-  return output
+  return resp
 
 def search_dirs_by_suffix(dirx, suffix):
   ''' return dirx's subdirs that contain files with the given suffix '''
