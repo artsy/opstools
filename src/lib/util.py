b9497d7f0acba7524ea981a8ddff2acb2652d98a
Jian <jian-feng@artsymail.com>
2023-07-01 18:15:53 -0400

    feat: add stub test files and makefile entries

--- src/lib/util.py
+++ src/lib/util.py
@@ -11,6 +11,10 @@ def list_intersect(list_a, list_b):
   ''' return elements common between l1 and l2 '''
   return [ x for x in list_a if x in list_b]
 
+def list_match_str(list_a, str1):
+  ''' return elements of list_a that match string str1 '''
+  return [x for x in list_a if str1 in x]
+
 def list_subtract(list_a, *args):
   '''
   return elements that are in list a but
@@ -20,10 +24,6 @@ def list_subtract(list_a, *args):
   for subtrahend in args:
     minuend = [x for x in minuend if x not in subtrahend]
   return minuend
-
-def list_match_str(list_a, str1):
-  ''' return elements of list_a that match string str1 '''
-  return [x for x in list_a if str1 in x]
 
 def parse_string_of_key_value_pairs(str1):
   '''
