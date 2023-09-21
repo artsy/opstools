f057c1db19bd0fd181345fe17a6bcea84ca7b336
Joey Aghion <joey@aghion.com>
2023-09-21 17:46:38 -0400

    feat: example script to bulk-update deprecated kubernetes specs

---
+++ src/hokusai-app-updater/examples/replace_deprecated_specs/replace_deprecated_specs.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Depends on GNU-sed installed as gsed (via `brew install gnu-sed`).
+
+deprecations=(
+#   "apiVersion: batch/v1beta1"
+  "apiVersion: networking.k8s.io/v1beta1"
+  "              serviceName: "
+  "              servicePort: "
+  "            serviceName: "
+  "            servicePort: "
+  "          serviceName: "
+  "          servicePort: "
+)
+
+replacements=(
+#   "apiVersion: batch/v1"
+  "apiVersion: networking.k8s.io/v1"
+  "              service:\n                name: "
+  "                port:\n                  name: "
+  "            service:\n              name: "
+  "              port:\n                name: "
+  "          service:\n            name: "
+  "            port:\n              name: "
+)
+
+for i in "${!deprecations[@]}"; do
+  echo "Replacing '${deprecations[i]}' with '${replacements[i]}'"
+
+  gsed -z -i -e "s|${deprecations[i]}|${replacements[i]}|" hokusai/staging.yml*
+  gsed -z -i -e "s|${deprecations[i]}|${replacements[i]}|" hokusai/production.yml*
+done
+
+
