cc2f274f257cca06847ca88bdcb3b341df1a3929
Jian <jian-feng@artsymail.com>
2023-10-02 15:22:19 -0400

    feat: script to migrate config secrets from k8s configmap to hashicorp vault

---
+++ src/migrate_config_secrets/migrate.py
@@ -0,0 +1,39 @@
+import argparse
+import logging
+import os
+
+import migrate_config_secrets.context
+
+from migrate_config_secrets.migrate import migrate_config_secrets
+from lib.logging import setup_logging
+
+
+def parse_args():
+  ''' parse command line args '''
+  parser = argparse.ArgumentParser(
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter
+  )
+  parser.add_argument(
+    'artsy_env',
+    choices=['staging', 'production'],
+    help='the artsy environment'
+  )
+  parser.add_argument(
+    '--loglevel',
+    choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
+    default='INFO',
+    help='log level'
+  )
+  return parser.parse_args()
+
+if __name__ == "__main__":
+
+  args = parse_args()
+  artsy_env, loglevel = (
+    args.artsy_env,
+    args.loglevel
+  )
+
+  setup_logging(eval('logging.' + loglevel))
+
+  migrate_config_secrets(artsy_env)
