0a0c9adc0e36f8c9c47182f8daa50ff2cc1ea1be
Jian <jian-feng@artsymail.com>
2023-11-22 16:12:37 -0500

    feat: add function to return all namespaces or by label

--- src/lib/test/test_k8s_namespaces.py
+++ src/lib/test/test_k8s_namespaces.py
@@ -8,7 +8,6 @@ from lib.test.fixtures.kctl import (
 from lib.k8s_namespaces import Namespaces
 
 def describe_namespaces():
-
   def describe_instantiation():
     def it_stores_namespaces(mock_kubectl_get_namespaces_json_object, mock_kctl):
       obj = Namespaces(mock_kctl)
@@ -36,11 +35,36 @@ def describe_namespaces():
       obj.delete('foonamespace')
       kctl_spy.assert_has_calls([mocker.call('foonamespace')])
 
+  def describe_namespaces():
+    def describe_no_labels_provided():
+      def it_returns_all_namespaces(mock_kctl):
+        obj = Namespaces(mock_kctl)
+        names = obj.namespaces()
+        assert names == ['namespace1', 'namespace2', 'namespace3']
+    def describe_one_label_provided():
+      def it_returns_matching_namespaces(mock_kctl):
+        obj = Namespaces(mock_kctl)
+        names = obj.namespaces(foo='bar')
+        assert names == ['namespace1', 'namespace2']
+      def it_returns_empty_when_no_namespace_matches(mock_kctl):
+        obj = Namespaces(mock_kctl)
+        names = obj.namespaces(foo='bing')
+        assert names == []
+      def it_converts_dashes_to_underscores(mock_kctl):
+        obj = Namespaces(mock_kctl)
+        names = obj.namespaces(foo_foo='bar')
+        assert names == ['namespace3']
+    def describe_multiple_labels_provided():
+      def it_returns_matching_namespaces(mock_kctl):
+        obj = Namespaces(mock_kctl)
+        names = obj.namespaces(foo='bar', bar='baz')
+        assert names == ['namespace1']
+
   def describe_old_namespaces():
     def it_returns_old_namespaces(mocker, mock_kctl):
       obj = Namespaces(mock_kctl)
       mocker.patch(
         'lib.k8s_namespaces.over_ndays_ago'
-      ).side_effect = [True, False]
+      ).side_effect = [True, False, False]
       names = obj.old_namespaces(10)
       assert names == ['namespace1']
