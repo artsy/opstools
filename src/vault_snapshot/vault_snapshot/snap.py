d02fc569f66be7bcc02dde3dca9b550266cb4ba1
Jian <jian-feng@artsymail.com>
2024-01-25 15:07:32 -0500

    feat: save snapshot to s3

--- src/vault_snapshot/vault_snapshot/snap.py
+++ src/vault_snapshot/vault_snapshot/snap.py
@@ -1,15 +1,52 @@
+import logging
+import os
+import shutil
+
+from distutils.dir_util import mkpath
+
 import vault_snapshot.context
 
+from lib.artsy_s3_backup import ArtsyS3Backup
 from lib.vault import Vault
 
 
 def take_snapshot(
   local_dir,
   artsy_env,
-  vault_addr,
+  vault_host,
+  vault_port,
   s3,
   s3_bucket,
   s3_prefix
 ):
-  vault_client = Vault(vault_addr, 'iam', role='opstools-role')
-  snapshot = vault_client.take_snapshot('/tmp/vault_snapshot.gz')
+  vault_client = Vault(
+    f'https://{vault_host}:{vault_port}',
+    'iam',
+    role='opstools-role'
+  )
+  export_dir = os.path.join(local_dir, artsy_env)
+  mkpath(export_dir)
+  file_name = f"{vault_host}.gz"
+  output_file = os.path.join(export_dir, file_name)
+  logging.info('Taking snapshot...')
+  vault_client.take_snapshot(output_file)
+  if s3:
+    try:
+      artsy_s3_backup = ArtsyS3Backup(
+        s3_bucket,
+        s3_prefix,
+        'vault',
+        artsy_env,
+        'gz'
+      )
+      logging.info('Backing up to S3...')
+      artsy_s3_backup.backup(output_file)
+    except:
+      raise
+    finally:
+      logging.info(f"Deleting {export_dir} ...")
+      shutil.rmtree(export_dir)
+  else:
+    logging.info(
+      "Skipping backup to S3. Please delete the local files when done!"
+    )
