5fbd3f22d10dd8aa7180371d9a8c192c47b0670d
Joey Aghion <joey@aghion.com>
2025-08-08 14:37:57 -0400

    feat: example of bulk-updating Claude safeguards

---
+++ src/hokusai-app-updater/examples/claude_settings/deny_remote_commands.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/env ruby
+require 'fileutils'
+require 'json'
+
+SETTINGS_FILENAME = ".claude/settings.json"
+DEFAULT_SETTINGS = {
+  permissions: {
+    allow: [],
+    deny: ["Read(.env)", "Read(.env.shared)"]
+  }
+}
+ADDITIONS = [
+  "Bash(hokusai:*)",
+  "Bash(aws:*)]"
+]
+
+# create settings file if it doesn't already exist
+unless File.exist?(SETTINGS_FILENAME)
+  FileUtils.mkdir_p(".claude")
+  File.open(SETTINGS_FILENAME, 'w') do |f|
+    f.write(JSON.pretty_generate(DEFAULT_SETTINGS))
+  end
+end
+
+# read in settings
+settings = JSON.parse(File.read(SETTINGS_FILENAME))
+
+# merge additions
+settings["permissions"]["deny"] |= ADDITIONS
+
+# write out updated settings
+File.open(SETTINGS_FILENAME, 'w') do |f|
+  f.write(JSON.pretty_generate(settings))
+end
+
